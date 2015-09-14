159759e0dc06b119d99e36953afa99b23ce1b876
Corey O'Brien <cobrien@sonomapartners.com>
2015-09-14 12:27:48 -0500

    Initial version

---
+++ ToDoList/www/scripts/ToDoList/AppViewModel.js
@@ -0,0 +1,73 @@
+﻿/// <reference path="Services.js" />
+/// <reference path="../knockout-3.3.0.js" />
+(function (global) {
+    "use strict";
+
+    var clientId = 'INSERT CLIENT ID HERE';
+    var redirectUri = 'http://localhost/callback';
+
+    global.ToDoList = global.ToDoList || {};
+
+    function AppViewModel() {
+        var self = this;
+        self.isAuthenticated = ko.observable(false);
+        self.newTaskSubject = ko.observable("");
+        self.newTaskDueDays = ko.observable("7");
+        self.tasks = ko.observableArray();
+        self.isNewTaskFormVisible = ko.observable(false);
+
+        self.showNewTaskForm = function showNewTaskForm() {
+            self.newTaskSubject("");
+            self.newTaskDueDays("7");
+            self.isNewTaskFormVisible(true);
+        };
+
+        self.saveNewTask = function saveNewTask() {
+            var dueDate = null;
+            if (self.newTaskDueDays()) {
+                dueDate = new Date();
+                dueDate.setDate(dueDate.getDate() + parseInt(self.newTaskDueDays()));
+                dueDate = dueDate.toISOString().substr(0, 10);
+            }
+            var newTask = {
+                Subject: self.newTaskSubject(),
+                ActivityDate: dueDate
+            };
+            ToDoList.Services.createRecord('Task', newTask).then(function onTaskCreated(task) {
+                self.isNewTaskFormVisible(false);
+                newTask.Id = task.id;
+                self.tasks.push(newTask);
+            });
+        };
+
+        self.cancelNewTask = function () {
+            self.isNewTaskFormVisible(false);
+        };
+
+        self.completeTask = function completeTask(task) {
+            ToDoList.Services.updateRecord('Task', task.Id, { Status: 'Completed' }).then(function () {
+                self.tasks.remove(task);
+            });
+
+            return true;
+        };
+
+        self.run = function run() {
+            ToDoList.Services.authenticate(clientId, redirectUri)
+                .then(function onAuthenticated() {
+                    self.isAuthenticated(true);
+
+                    var query =
+                        "SELECT Id, Subject, ActivityDate FROM Task WHERE IsClosed=FALSE AND OwnerId='" +
+                        ToDoList.Services.getCurrentUserId() +
+                        "' ORDER BY ActivityDate";
+
+                    ToDoList.Services.getQueryResults(query, self.tasks);
+                });
+        }
+    };
+
+    global.ToDoList.AppViewModel = AppViewModel;
+
+
+})(window);
---
+++ ToDoList/www/scripts/ToDoList/Services.js
@@ -0,0 +1,94 @@
+﻿/// <reference path="../knockout-3.3.0.js" />
+(function (global) {
+    "use strict";
+
+    global.ToDoList = global.ToDoList || {};
+    global.ToDoList.Services = global.ToDoList.Services || {};
+
+    var oauthResult = null;
+
+    function authenticate(clientId, redirectUri) {
+        var deferred = $.Deferred();
+
+        var url = [
+			'https://login.salesforce.com/services/oauth2/authorize?response_type=token&display=touch&client_id=',
+			encodeURIComponent(clientId),
+			'&redirect_uri=',
+			encodeURIComponent(redirectUri)
+        ].join('');
+
+        var loginWindow = window.open(url, "_blank", "location=no");
+
+        loginWindow.addEventListener('loadstart', function (event) {
+            if (event.url.indexOf(redirectUri) == 0) {
+                var fragment = event.url.split('#')[1]
+                oauthResult = {};
+                var pairs = fragment.split('&');
+                for (var i in pairs) {
+                    var keyAndValue = pairs[i].split('=');
+                    oauthResult[decodeURIComponent(keyAndValue[0])] = decodeURIComponent(keyAndValue[1]);
+                }
+
+                loginWindow.close();
+                deferred.resolve(oauthResult);
+            }
+        });
+
+        return deferred.promise();
+    };
+
+    function restApiRequest(path, method, data, contentType) {
+        return $.ajax(
+			oauthResult.instance_url + '/services/data/v34.0' + path,
+			{
+			    method: method,
+			    dataType: 'json',
+			    contentType: contentType,
+			    data: data,
+			    headers: { 'Authorization': 'Bearer ' + oauthResult.access_token }
+			}
+		).error(function (error) { console.log(JSON.stringify(error)); });
+    };
+
+    function getQueryResults(query, target) {
+        console.log('Executing query: ' + query);
+
+        return restApiRequest('/query/', 'GET', { q: query })
+			.then(function onQueryResults(results) {
+			    console.log('query results: ' + JSON.stringify(results));
+
+			    if (ko.isWriteableObservable(target))
+			        target(results.records);
+
+			    return results;
+			});
+    }
+
+    function updateRecord(objectName, objectId, fieldValues) {
+        return restApiRequest(
+			'/sobjects/' + objectName + '/' + objectId,
+			'PATCH',
+			JSON.stringify(fieldValues),
+			'application/json');
+    };
+
+    function createRecord(objectName, fieldValues) {
+        return restApiRequest(
+			'/sobjects/' + objectName + '/',
+			'POST',
+			JSON.stringify(fieldValues),
+			'application/json');
+    };
+
+    function getCurrentUserId() {
+        var index = oauthResult.id.lastIndexOf('/');
+        return oauthResult.id.substr(index + 1);
+    }
+
+    ToDoList.Services.authenticate = authenticate;
+    ToDoList.Services.getQueryResults = getQueryResults;
+    ToDoList.Services.getCurrentUserId = getCurrentUserId;
+    ToDoList.Services.updateRecord = updateRecord;
+    ToDoList.Services.createRecord = createRecord;
+
+})(window);
